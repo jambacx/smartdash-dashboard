a9df09fc0c9ea1d300ff78b9bf8429cf4765da2f
JambaCX <jamba@mobicom.mn>
2023-11-08 17:54:23 +0800

    export fixed

--- src/components/export/ExportDownload.tsx
+++ src/components/export/ExportDownload.tsx
@@ -2,14 +2,48 @@ import React from 'react';
 import Papa from 'papaparse';
 import { CloudDownloadRounded } from '@mui/icons-material';
 import { Button, CircularProgress } from '@mui/material';
+import moment from 'moment'
+const processData = (data: any, title: any) => {
+  return data.map((item: any) => {
+    const createdTime = moment.unix(item?.created_time).format('YYYY-MM-DD HH:mm');
+    const expireTime = moment.unix(item?.expire_time).format('YYYY-MM-DD HH:mm');
+
+    const itemWithoutEmojis = {
+      ...item,
+      created_time: createdTime,
+      expire_time: expireTime,
+    };
+
+
+    if (title === 'Нийтлэл') {
+      const updatedTime = moment.unix(item?.updated_time).format('YYYY-MM-DD HH:mm');
+      const emojiCounts = {
+        ANGRY: parseInt(item.ANGRY) || 0,
+        LIKE: parseInt(item.LIKE) || 0,
+        HAHA: parseInt(item.HAHA) || 0,
+        SAD: parseInt(item.SAD) || 0,
+        WOW: parseInt(item.WOW) || 0,
+        CARE: parseInt(item.CARE) || 0,
+        LOVE: parseInt(item.LOVE) || 0,
+      };
+
+      return {
+        ...itemWithoutEmojis,
+        ...emojiCounts,
+        updated_time: updatedTime,
+      };
+    }
+
+    return itemWithoutEmojis;
+  });
+};
 
 const CsvDownload = ({ title, data, loading }: { title: string, data: any, loading: any }) => {
   const handleDownload = () => {
-
-    const csv = Papa.unparse(data);
+    const processValue = processData(data, title)
+    const csv = Papa.unparse(processValue);
     const BOM = "\uFEFF";
     const csvData = new Blob([BOM + csv], { type: 'text/csv;charset=utf-8;' });
-
     const csvUrl = URL.createObjectURL(csvData);
     const tempLink = document.createElement('a');
     tempLink.href = csvUrl;
@@ -18,8 +52,6 @@ const CsvDownload = ({ title, data, loading }: { title: string, data: any, loadi
     tempLink.click();
     document.body.removeChild(tempLink);
   };
-
-
 
   return (
     <Button
